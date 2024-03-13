1451edbcb1c0ec10cf2772faefb0cd0b31721e00
Alan Rodriguez <60365507+errror-404@users.noreply.github.com>
2024-03-12 19:26:11 -0700

    dashboard layout added

---
+++ app/(dashboard)/_components/sidebar/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Sidebar = () => {
+  return (
+    <aside className=" text-white fixed z-[1] left-0 bg-blue-950 h-full w-[60px] flex flex-col p-3 gap-y-4">
+      Side
+    </aside>
+  );
+};
+
+export default Sidebar;
